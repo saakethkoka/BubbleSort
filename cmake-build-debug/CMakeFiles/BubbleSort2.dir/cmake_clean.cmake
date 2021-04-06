b07767cc3e162e58715ce2bc5221ebc990f07c85
saakethkoka <saaketh.koka@gmail.com>
2021-04-06 13:05:14 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/BubbleSort2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BubbleSort2"
+  "BubbleSort2.pdb"
+  "CMakeFiles/BubbleSort2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BubbleSort2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BubbleSort2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BubbleSort2.
+# This may be replaced when dependencies are built.
